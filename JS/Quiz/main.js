f0210477eca7b92c42c4e0824cdbb108f8c3b098
yota-g <yota18493@gmail.com>
2021-07-07 23:30:15 +0900

    quia learning

---
+++ JS/Quiz/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+
+  const quizSet = [
+    {q: 'What is A?', c: ['A0', 'A1', 'A2']},
+    {q: 'What is B?', c: ['B0', 'B1', 'B2']},
+    {q: 'What is C?', c: ['C0', 'C1', 'C2']},
+  ];
+
+  //何問目なのかを確認できるようにその情報を配列で持っておく。
+  let currentNum = 0;
+
+  question.textContent = quizSet[currentNum].q;
+
+  quizSet[currentNum].c.forEach( choice => {
+    const li = document.createElement('li')
+    li.textContent = choice;
+    choices.appendChild(li);
+  });
+}
