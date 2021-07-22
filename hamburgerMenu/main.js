94dbdee270e91f3b396197b25d2fb4b94594b4b9
yota-g <yota18493@gmail.com>
2021-07-22 22:43:01 +0900

    main.js created

---
+++ hamburgerMenu/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+open.addEventListener('click', ()=>{
+
+});
+  const close = document.getElementById('close');
+  document.addEventListenre('click', () => {
+
+  })
+}
