dcf3081c34651c1a46ca72447e686374c3d40b16
yota-g <yota18493@gmail.com>
2021-07-22 17:27:22 +0900

    paddle scope create:

--- Pinpong Game/main.js
+++ Pinpong Game/main.js
@@ -6,8 +6,8 @@
     //maxまでのran数を設定する。
     //min以上にするためにmin を加える。
   }
-  class Ball{
-    constructor(canvas){
+  class Ball {
+    constructor(canvas) {
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
       // this.x = 30;
@@ -15,31 +15,29 @@
       this.y = 30;
       this.r = 10;
       // this.vx = 5;
-      this.vx = rand(3, 5) * (Math. random() < 0.5 ? 1 : -1);
+      this.vx = rand(3, 5) * (Math.random() < 0.5 ? 1 : -1);
       // this.vy = 3;
-      this.vy = rand(3, 5) *(Math.random() < 0.5 ? 1 : -1);
+      this.vy = rand(3, 5) * (Math.random() < 0.5 ? 1 : -1);
     }
 
-    update(){
+    update() {
       this.x += this.vx;
       this.y += this.vy;
       //壁に当たったら跳ね返るようにする。
       //半径を考慮しないと壁に食い込んだ状態になるのでそこを反映させる。
-      if(
-        this.x - this.r< 0 || this.x + this.r > this.canvas.width
-      ){
+      if (this.x - this.r < 0 || this.x + this.r > this.canvas.width) {
         this.vx *= -1;
       }
 
-      if( this.y - this.r < 0 || this.y + this.r > this.canvas.height ){
+      if (this.y - this.r < 0 || this.y + this.r > this.canvas.height) {
         this.vy *= -1;
       }
     }
 
-    draw(){
+    draw() {
       this.ctx.beginPath();
       this.ctx.fillStyle = '#fdfdfd';
-      this.ctx.arc(this.x, this.y, this.r, 0 ,2 * Math.PI);
+      this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
       this.ctx.fill();
     }
   }
@@ -47,31 +45,50 @@
   class Paddle {
     constructor(canvas) {
       this.canvas = canvas;
-      this.ctx = canvas.getContext('2d');
+      this.ctx = this.canvas.getContext('2d');
       this.w = 60;
       this.h = 16;
-      this.x = this.canvas.width /2 - (this.w / 2) ;
+      this.x = this.canvas.width / 2 - (this.w / 2);
       this.y = this.canvas.height - 32;
+      this.mouseX = this.x;
+      this.addHandler();
     }
-    update(){
 
+    addHandler() {
+      document.addEventListener('mousemove', (e) => {
+        this.mouseX = e.clientX;
+        //マウスと壁の位置がズレている。これはclientXの位置がブラウザの端を視点にしているから、その分を引く。
+      });
     }
-    draw(){
+
+    update() {
+      const rect = this.canvas.getBoundingClientRect();
+      this.x = this.mouseX - rect.left - (this.w / 2);
+      // -this.wはpaddleの左端がマウスの座標になるのでパドルの真ん中にマウスの座標がくるようにしている。
+      if (this.x < 0 ){
+        this.x = 0;
+      }
+      if (this.x > this.canvas.width){
+        this.x = this.canvas.width - this.w
+      }
+    }
+
+    draw() {
       this.ctx.fillStyle = '#fdfdfd';
-      this.ctx.fillRect(this.x , this.y, this.w, this.h);
+      this.ctx.fillRect(this.x, this.y, this.w, this.h);
     }
   }
 
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
-      this.ctx = canvas.getContext('2d');  
+      this.ctx = this.canvas.getContext('2d');
       this.ball = new Ball(this.canvas);
-      this.paddle = new Paddle (this.canvas);
+      this.paddle = new Paddle(this.canvas);
       this.loop();
     }
     //情報を更新して描画するのをゲームループと呼ぶ。
-    loop(){
+    loop() {
       this.update();
       this.draw();
 
@@ -87,7 +104,7 @@
       this.paddle.update();
     }
 
-    draw(){
+    draw() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.ball.draw();
       this.paddle.draw();
@@ -96,8 +113,8 @@
   }
 
   const canvas = document.querySelector('canvas');
-  if(typeof canvas.getContext === 'undefined'){
-    return ; //関数の中でしか使用できないのでブロックを即時関数に変更。
+  if (typeof canvas.getContext === 'undefined') {
+    return; //関数の中でしか使用できないのでブロックを即時関数に変更。
   }
 
   new Game(canvas);
