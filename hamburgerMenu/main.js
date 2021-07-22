fecd58a1f31f4a5b1e33ef32aec63db452964edc
yota-g <yota18493@gmail.com>
2021-07-22 23:25:30 +0900

    create hamburgerMenu

--- hamburgerMenu/main.js
+++ hamburgerMenu/main.js
@@ -4,13 +4,14 @@
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
   const close = document.getElementById('close');
-  open.addEventListener('click', () => {
-    document.addEventListener('click', () => {
-      overlay.classList.add('show');
-      open.classList.add('hide');
-    });
-  });
-  close.addEventListener('click', () => {
 
-  })
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+  
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
 }
