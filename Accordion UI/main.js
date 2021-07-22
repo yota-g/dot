2e096239969ca9ccb5e230068199f560839a0cf6
yota-g <yota18493@gmail.com>
2021-07-23 00:17:01 +0900

    according ul learned

--- Accordion UI/main.js
+++ Accordion UI/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
 {
+  const dts = document.querySelectorAll('dt');
   
+  dts.forEach(dt => {
+    dt.addEventListener('click', () => {
+      dt.parentNode.classList.toggle('appear');
+      //dtの親要素にappearをつけはずしをする。
+      dts.forEach(el => {
+        if (dt != el){
+          el.parentNode.classList.remove('appear');
+        }
+      });
+    });
+  });
 }
