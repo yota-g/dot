63335575473aa2929168d2de13f608f9dcd381c1
yota-g <yota18493@gmail.com>
2021-07-23 11:23:29 +0900

    collect common function

--- Carousel/main.js
+++ Carousel/main.js
@@ -11,14 +11,17 @@
   function updateButtons() {
     prev.classList.remove('hidden');
     next.classList.remove('hidden');
-
     if(currentIndex === 0) {
       prev.classList.add('hidden');
     }
-
     if(currentIndex === slides.length - 1){
       next.classList.add('hidden');
     }
+  }
+
+  function moveSlides() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX( ${-1 * slideWidth * currentIndex}px)`;
   }
 
   updateButtons();
@@ -27,16 +30,18 @@
   next.addEventListener('click', () => {
     currentIndex++;
     updateButtons();
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    //getBoundingClientRectで要素に関する情報を取得。その中のwidthプロパティを使用すればOK
-    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex }px)`;
-    //ulをx軸方向に移動させる。 マイナス方向に移動させる。移動させる距離に対して何番目に移動させるかをかければいい。clickする度に増えるようにすればいい。
+    moveSlides();
+    // const slideWidth = slides[0].getBoundingClientRect().width;
+    // //getBoundingClientRectで要素に関する情報を取得。その中のwidthプロパティを使用すればOK
+    // ul.style.transform = `translateX(${-1 * slideWidth * currentIndex }px)`;
+    // //ulをx軸方向に移動させる。 マイナス方向に移動させる。移動させる距離に対して何番目に移動させるかをかければいい。clickする度に増えるようにすればいい。
   });
 
   prev.addEventListener('click', () =>{
     currentIndex --;
     updateButtons();
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    ul.style.transform = `translateX( ${-1 * slideWidth * currentIndex}px)`;
+    moveSlides();
+    // const slideWidth = slides[0].getBoundingClientRect().width;
+    // ul.style.transform = `translateX( ${-1 * slideWidth * currentIndex}px)`;
   });
 }
