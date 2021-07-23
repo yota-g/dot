d1eb74d9fd647b8ed7dbf966a2dc0d4740603522
yota-g <yota18493@gmail.com>
2021-07-23 11:01:40 +0900

    create next button function

--- Carousel/main.js
+++ Carousel/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 {
+  const next = document.getElementById('next');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
+  //一旦全てのスライドを取得しそれを使用して使っていく。。
   
+  next.addEventListener('click', () => {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    //getBoundingClientRectで要素に関する情報を取得。その中のwidthプロパティを使用すればOK
+    ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+    //ulをx軸方向に移動させる。 マイナス方向に移動させる。
+
+  })
 }
