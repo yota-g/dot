2388193600afc53334c25234283193b29856e778
yota-g <yota18493@gmail.com>
2021-07-23 11:55:47 +0900

    carousel learned

--- Carousel/main.js
+++ Carousel/main.js
@@ -7,6 +7,7 @@
   const slides = ul.children;
   //一旦全てのスライドを取得しそれを使用して使っていく。。
   let currentIndex = 0;
+  const dots = [];
 
   function updateButtons() {
     prev.classList.remove('hidden');
@@ -24,12 +25,36 @@
     ul.style.transform = `translateX( ${-1 * slideWidth * currentIndex}px)`;
   }
 
+  function setupDots() {
+    for (let i = 0; i < slides.length; i ++){
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        currentIndex = i;
+        updateDots();
+        updateButtons();
+        moveSlides();
+      })
+      dots.push(button);
+      // 後で処理ができるようにdosに格納
+      document.querySelector('nav').appendChild(button);
+    }
+    dots[0].classList.add('current');
+  }
+
+  function updateDots() {
+    dots.forEach( dot => {
+      dot.classList.remove('current');
+    })
+    dots[currentIndex].classList.add('current');
+  }
   updateButtons();
-  //ページを読み込んだ時にcurrentIndexが0で必要だから読み込む。
+  //ページを読み込んだ時にcurrentIndexが0で必要
+  setupDots();
   
   next.addEventListener('click', () => {
     currentIndex++;
     updateButtons();
+    updateDots();
     moveSlides();
     // const slideWidth = slides[0].getBoundingClientRect().width;
     // //getBoundingClientRectで要素に関する情報を取得。その中のwidthプロパティを使用すればOK
@@ -40,8 +65,14 @@
   prev.addEventListener('click', () =>{
     currentIndex --;
     updateButtons();
+    updateDots();
     moveSlides();
     // const slideWidth = slides[0].getBoundingClientRect().width;
     // ul.style.transform = `translateX( ${-1 * slideWidth * currentIndex}px)`;
+  });
+
+  window.addEventListener('resize', () => {
+    moveSlides();
+    // スライドの幅を再計算することで幅を変えたときに他の要素が入り込むのを防ぐことができる。
   });
 }
